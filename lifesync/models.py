e2f416c70412d50cd230706f11d7867ed21bc784
Henrique Sebastião <contato@henriquesebastiao.com>
2025-08-06 13:47:31 -0400

    feat: Add accounts table

--- lifesync/models.py
+++ lifesync/models.py
@@ -1,9 +1,9 @@
 from datetime import datetime
-from typing import Optional
+from typing import List, Optional
 from zoneinfo import ZoneInfo
 
-from sqlalchemy import func
-from sqlalchemy.orm import Mapped, mapped_column, registry
+from sqlalchemy import ForeignKey, UniqueConstraint, func
+from sqlalchemy.orm import Mapped, mapped_column, registry, relationship
 
 table_registry = registry()
 
@@ -20,6 +20,14 @@ class User:
     first_name: Mapped[str]
     last_name: Mapped[Optional[str]] = mapped_column(default=None)
     created_at: Mapped[datetime] = mapped_column(init=False, server_default=func.now())
+
+    accounts: Mapped[List['Account']] = relationship(
+        back_populates='user',
+        lazy='immediate',
+        init=False,
+        default_factory=list,
+        cascade='delete',
+    )
 
 
 @table_registry.mapped_as_dataclass
@@ -50,3 +58,19 @@ class Quote:
     updated_at: Mapped[datetime] = mapped_column(
         init=False, server_default=func.now(), onupdate=datetime.now(tz=ZoneInfo('UTC'))
     )
+
+
+@table_registry.mapped_as_dataclass
+class Account:
+    __tablename__ = 'accounts'
+    __table_args__ = (
+        # Cada usuário só possa ter uma conta com determinado nome
+        UniqueConstraint('account_holder_id', 'name', name='uix_user_account_name'),
+    )
+
+    id: Mapped[int] = mapped_column(init=False, primary_key=True)
+    name: Mapped[str]
+    balance: Mapped[float]
+    account_holder_id: Mapped[int] = mapped_column(ForeignKey('users.id'))
+
+    user: Mapped['User'] = relationship(back_populates='accounts', lazy='immediate', init=False)
