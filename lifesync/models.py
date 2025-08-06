a00b28e4bd83086ca59f9905644a8a242dfa9c61
Henrique Sebastião <contato@henriquesebastiao.com>
2025-08-06 07:02:28 -0400

    db: Creating table quotes - close #6

--- lifesync/models.py
+++ lifesync/models.py
@@ -9,6 +9,8 @@ table_registry = registry()
 
 @table_registry.mapped_as_dataclass
 class User:
+    """Tabela de usuários do app"""
+
     __tablename__ = 'users'
 
     id: Mapped[int] = mapped_column(init=False, primary_key=True)
@@ -17,3 +19,30 @@ class User:
     first_name: Mapped[str]
     last_name: Mapped[Optional[str]] = mapped_column(default=None)
     created_at: Mapped[datetime] = mapped_column(init=False, server_default=func.now())
+
+
+@table_registry.mapped_as_dataclass
+class Quotes:
+    """Tabela de cotações de moedas consultadas na API AwesomeAPI
+
+    Attributes:
+        id (int): Chave primária
+        code (str): Código de moeda
+        high (float): Máximo
+        low (float): Mínimo
+        varBid (float): Variação
+        pctChange (float): Porcentagem de Variação
+        bid (float): Compra
+        ask (float): Venda
+    """
+
+    __tablename__ = 'quotes'
+
+    id: Mapped[int] = mapped_column(init=False, primary_key=True)
+    code: Mapped[str]
+    high: Mapped[float]
+    low: Mapped[float]
+    varBid: Mapped[float]
+    pctChange: Mapped[float]
+    bid: Mapped[float]
+    ask: Mapped[float]
---
+++ lifesync/schemas/finance/quotes.py
@@ -0,0 +1,24 @@
+from pydantic import BaseModel
+
+
+class QuoteCreate(BaseModel):
+    code: str
+    high: float
+    low: float
+    varBid: float
+    pctChange: float
+    bid: float
+    ask: float
+
+
+class QuoteUpdate(BaseModel):
+    high: float | None = None
+    low: float | None = None
+    varBid: float | None = None
+    pctChange: float | None = None
+    bid: float | None = None
+    ask: float | None = None
+
+
+class QuoteSchema(QuoteCreate):
+    id: int
